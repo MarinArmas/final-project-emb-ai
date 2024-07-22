d24a183db52bbef8373f203c697b25b1897072a7
user <email@example.com>
2024-07-22 18:05:57 -0400

    finished the lab

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,33 +1,28 @@
-"""
-Emotion detector 
-"""
-# Import the requests library
 import requests, json
 
-# Define the "emotion_detector" function, which receives as an argument "text_to_analyze"
 def emotion_detector(text_to_analyze):
-    """
-    emotion_detector function
-    Input: text_to_analyze
-    Output: response.text
-    """
-    # Define the Watdon NLP URL
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    # Define the headers
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
-    # Create a request and store the response in "response" variable
     response = requests.post(url, json = myobj, headers = header)
-    formatted_response = json.loads(response.text)
 
-    emotion_scores  = formatted_response['emotionPredictions'][0]['emotion']
-    anger_score = emotion_scores['anger']
-    disgust_score = emotion_scores['disgust']
-    fear_score = emotion_scores['fear']
-    joy_score = emotion_scores['joy']
-    sadness_score = emotion_scores['sadness']
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        emotion_scores  = formatted_response['emotionPredictions'][0]['emotion']
+        anger_score = emotion_scores['anger']
+        disgust_score = emotion_scores['disgust']
+        fear_score = emotion_scores['fear']
+        joy_score = emotion_scores['joy']
+        sadness_score = emotion_scores['sadness']
+        dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+    elif response.status_code == 400:
+        anger_score = None
+        disgust_score = None
+        fear_score = None
+        joy_score = None
+        sadness_score = None
+        dominant_emotion = None
 
-    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
     formatted_data = {
         'anger': anger_score,
         'disgust': disgust_score,
@@ -36,5 +31,5 @@ def emotion_detector(text_to_analyze):
         'sadness': sadness_score,
         'dominant_emotion': dominant_emotion
     }
-    # Return the response as text
+    
     return formatted_data
